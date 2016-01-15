c829cb32f5656eee03e81c4dedec782b39251fab
alumne1daw <alumne1daw@localhost>
2016-01-15 15:25:38 +0100

    Commit: Programa finalizado - En proceso de documentacion

--- SubvencionsNadal/src/main/java/net/javierjimenez/SubvencionsNadal/Personatge.java
+++ SubvencionsNadal/src/main/java/net/javierjimenez/SubvencionsNadal/Personatge.java
@@ -1,26 +1,40 @@
 package net.javierjimenez.SubvencionsNadal;
 
+/**
+ * 
+ * @author alumne1daw
+ *
+ */
 public class Personatge {
 	
+	/**
+	 * Variable String que contindra l'identificador (nom) de l'objecte Personatge.
+	 */
 	private String nom;
 	
+	/**
+	 * Variable int que conté el total de regals que te l'objecte Personatge
+	 */
 	private int regals;
 	
+	/**
+	 * 
+	 * 
+	 * @param n
+	 * @param r
+	 */
 	public Personatge(String n, int r){
 		
 		nom = n;
 		regals = r;
 		
 	}
-	
 	public void sumarRegals(int r){
 		regals = regals + r;
 	}
-
 	public int getRegals(){
 		return regals;
 	}
-	
 	public String getNom() {
 		return nom;
 	}
